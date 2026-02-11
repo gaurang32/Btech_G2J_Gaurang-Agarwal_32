da9ccd84177714878642cd31e9a86dc35c01b80d
gaurang32 <gaurangagarwal324@gmail.com>
2026-02-11 10:47:34 +0530

    Add files via upload

---
+++ eqaual number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+ {
+    int n1,n2;
+    printf("Enter first number: ");
+    scanf("%d",&n1);
+    printf("Enter second number: ");
+    scanf("%d",&n2);
+    if (n1 == n2)
+     {
+    printf("Both numbers are same.\n");
+    } else 
+    {
+     printf("Numbers are different.\n");
+    }
+    return 0;
+}
