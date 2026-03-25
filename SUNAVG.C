2a7d83871118421e34a87c1fb9cdba9072d1b98f
gaurang32 <gaurangagarwal324@gmail.com>
2026-03-25 09:29:47 +0530

    Add files via upload

---
+++ SUNAVG.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    printf("Sum: %.2f\n", sum);
+    printf("Average: %.2f", sum / n);
+    return 0;
+}
